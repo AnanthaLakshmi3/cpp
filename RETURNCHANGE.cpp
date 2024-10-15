73bf47b8a3065f3ee4acbaaf0d8cc27a8e6b381f
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-15 19:16:17 +0530

    Add files via upload

---
+++ RETURNCHANGE.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    int c=((x+5)/10)*10;
+	    int change=100-c;
+	    cout << change << endl;
+	}
+
+}
