c98c319b06d6300355e30e33685a6d7662bb1973
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-15 19:16:45 +0530

    Add files via upload

---
+++ DIFFSUM.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	int total;
+	cin >> x >> y;
+	if (x>y){
+	    total=x-y;
+	    cout << total; 
+	}
+	else{
+	    total=x+y ;
+	    cout << total;
+	}
+
+}
