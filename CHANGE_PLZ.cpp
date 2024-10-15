32ccbf9f3e6e2d16a125a3430d5ceb3d3d734d12
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-15 19:17:02 +0530

    Add files via upload

---
+++ CHANGE_PLZ.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    int change;
+	    change=(100-x)/10*10;
+	    cout << change << endl;
+	}
+
+}
