675d62da66b959839400e7ef1b85652c9595c4a6
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-16 19:25:42 +0530

    Add files via upload

---
+++ FLOW008.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if (x<10){
+            cout << "Thanks for helping Chef!" << endl ;
+        }
+        else{
+            cout << "-1" << endl;
+        }
+    }
+}
