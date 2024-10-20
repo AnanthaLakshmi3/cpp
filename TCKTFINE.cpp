cc5f679c6ddbb58e7941e5dcb026b6166cf10dea
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-20 19:35:27 +0530

    Add files via upload

---
+++ TCKTFINE.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y,z;
+	    cin >> x >> y >> z;
+	    int p=y-z;
+	    int m=x*p;
+	    cout << m << endl;
+	}
+
+}
