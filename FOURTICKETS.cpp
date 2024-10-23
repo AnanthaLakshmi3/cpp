4fbbfc826cc1d2239b867654d3f4745d50a1f89e
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-23 21:49:52 +0530

    Add files via upload

---
+++ FOURTICKETS.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    int k=x*4;
+	    if (k<=1000){
+	        cout << "YES" << endl;
+	    }
+	    else{
+	        cout << "NO" << endl;
+	    }
+	}
+
+}
