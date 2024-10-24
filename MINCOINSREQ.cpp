b07ea211986f7d71d8f948d9567174ace09e9c6c
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-24 20:57:21 +0530

    Add files via upload

---
+++ MINCOINSREQ.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >>t;
+    while(t--){
+        int x;
+        cin >> x;
+        int coins=x%10;
+        cout << coins << endl;
+    }
+}
