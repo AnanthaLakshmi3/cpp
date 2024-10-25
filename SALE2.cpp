8652bbaef793860b5ac9aa56bc5104cb86d7385a
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-25 19:12:48 +0530

    Add files via upload

---
+++ SALE2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y;
+	    cin >> x >>y;
+	    int z=x/3;
+	    int p=x-z;
+	    cout << p*y << endl;
+	}
+	
+
+}
