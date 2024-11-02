c4b626882169b535f1ad43a18a7a210144a73105
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-11-02 20:14:41 +0530

    Add files via upload

---
+++ CHEFCHOCO.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int c,x,y,z;
+	    cin >> c >> x>> y;
+	    z=c-x;
+	    cout << z*y << endl;
+	}
+
+}
