31315d36921b2db6eba64bde8fe26aee19dda043
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-11-02 20:14:05 +0530

    Add files via upload

---
+++ JCOINS.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y;
+	    cin >> x >> y;
+	    int z=x*10;
+	    int k=y*5;
+	    cout << z+k << endl;
+	}
+
+}
