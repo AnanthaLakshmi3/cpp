4de37b288a7765c1bfd19c945ae66e2116f817dd
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-11-03 20:16:07 +0530

    Add files via upload

---
+++ AIANALYSE.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ;
+	cin >> t;
+	if (t<=1000){
+	    cout << "Yes" << endl;
+	}
+	else{
+	    cout << "No" << endl;
+	}
+
+}
