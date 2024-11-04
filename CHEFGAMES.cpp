d3ce5ce5f70d5b0258001dbf7f4774501002b2bd
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-11-04 21:58:48 +0530

    Add files via upload

---
+++ CHEFGAMES.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int a,b,c,d;
+	    cin >> a >>b >>c >>d;
+	    if (a==0 and b==0 and c==0 and d==0){
+	        cout << "IN" << endl;
+	    }
+	    else{
+	        cout << "OUT" << endl;
+	    }
+	}
+
+}
