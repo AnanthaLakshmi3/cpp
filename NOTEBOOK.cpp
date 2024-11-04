1d643ac42f73762fa27633daebf636a5a032974d
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-11-04 21:57:20 +0530

    Add files via upload

---
+++ NOTEBOOK.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    int z=x*1000;
+	    int k=z/100;
+	    cout << k<< endl;
+	}
+
+}
