a6d33a088f4f930174ee596ea0c2158a98d47ea2
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-11-15 19:39:20 +0530

    Add files via upload

---
+++ ENSPACE.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y,z;
+	    cin >> x >> y >> z;
+	    int k=y+2*z;
+	    if (k<=x){
+	        cout << "Yes" << endl;
+	    }
+	    else{
+	        cout << "No" << endl;
+	    }
+	}
+
+}
